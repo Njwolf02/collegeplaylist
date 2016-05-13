f6537aba49704a14497e7b8b9d19d6b99ecf7449
Nicolas Wolf <njwolf02@louisville.edu>
2016-05-13 02:46:48 -0400

    mcv 4.5 update

---
+++ collegeplaylist.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(collegeplaylist.WebUI.Startup))]
+namespace collegeplaylist.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
